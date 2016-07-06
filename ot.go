20fa29b096519257d10f644cde0a71f9aa928fc9
k <phosphoresce.k@gmail.com>
2016-07-06 10:39:09 -0500

    sloppy prototype...

--- ot.go
+++ ot.go
@@ -12,20 +12,17 @@ func main() {
 }
 
 func _main() {
-	var zcount, xcount int
-	var totaltaps, deviation, gap float64
-	var grade string
-	tpm := 0.0
+	var opt int
 
-	// init ncurses menu here
+	// log file
 	f, err := os.Create("ot.log")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-
 	log.SetOutput(f)
 
+	// init ncurses window
 	text, err := gc.Init()
 	if err != nil {
 		log.Println("Init: ", err)
@@ -36,6 +33,61 @@ func _main() {
 	gc.Cursor(0)
 	gc.Raw(true)
 	gc.Echo(false)
+
+	// menu
+	text.Clear()
+	text.Keypad(true)
+	menu_items := []string{"Test your skill", "140 bpm", "160 bpm", "180 bpm", "200 bpm", "Exit"}
+	items := make([]*gc.MenuItem, len(menu_items))
+	for i, val := range menu_items {
+		items[i], _ = gc.NewItem(val, "")
+		defer items[i].Free()
+	}
+	menu , err := gc.NewMenu(items)
+	if err != nil {
+		text.Print(err)
+	}
+	defer menu.Free()
+	menu.Post()
+	for {
+		gc.Update()
+		ch := text.GetChar()
+
+		switch gc.KeyString(ch) {
+		case "q":
+			os.Exit(0)
+		case "down":
+			menu.Driver(gc.REQ_DOWN)
+			opt++
+		case "up":
+			menu.Driver(gc.REQ_UP)
+			opt--
+		case "return", "enter":
+			text.Erase()
+			text.Keypad(false)
+			switch opt {
+			case 0:
+				testYourSkill(text)
+			case 1:
+				testBpm(text, 140.00)
+			case 2:
+				testBpm(text, 160.00)
+			case 3:
+				testBpm(text, 180.00)
+			case 4:
+				testBpm(text, 200.00)
+			case 5:
+				os.Exit(0)
+			}
+		}
+	}
+}
+
+func testYourSkill(text *gc.Window) {
+	var zcount, xcount, row int
+	var totaltaps, deviation, gap float64
+	var grade string
+	tpm := 0.0
 
 	// start the clock
 	start := time.Now()
@@ -48,10 +100,9 @@ func _main() {
 		text.MovePrintf(3, 0, "1/2 note streams at %6.2f bpm", tpm/2)
 		text.MovePrintf(4, 0, "1/4 note streams at %6.2f bpm", tpm/4)
 		text.MovePrintf(6, 0, "Time between taps:")
-		text.MovePrintf(7 + zcount + xcount, 0, "%8.2f ms [%s]", gap, grade)
 		text.Refresh()
 
-		handleInput(text, &zcount, &xcount, &start, &lastTap, &gap)
+		handleInput(text, &zcount, &xcount, &row, &start, &lastTap, &gap)
 		totaltaps = float64(zcount + xcount)
 		tpm = totaltaps/time.Now().Sub(start).Minutes()
 		deviation = 1000/(tpm/60)
@@ -60,10 +111,51 @@ func _main() {
 		} else {
 			grade = "bad"
 		}
+		text.MovePrintf(7 + row, 0, "%8.2f ms [%s]", gap, grade)
+		row++
+		if row >= 50 {
+			row = 0
+		}
 	}
 }
 
-func handleInput(text *gc.Window, zcount, xcount *int, start, lastTap *time.Time, gap *float64) {
+func testBpm(text *gc.Window, bpm float64) {
+	var zcount, xcount, row int
+	var totaltaps, deviation, gap float64
+	var grade string
+	tpm := 0.0
+	deviation = 1000/((bpm*4)/60)
+
+	// start the clock
+	start := time.Now()
+	lastTap := start
+
+	for {
+		// updateConsole
+		text.MovePrintf(0, 0, "tpm: %6.2f z: %6d x: %6d", tpm, zcount, xcount)
+		text.MovePrintf(2, 0, "%v bpm mode", bpm)
+		text.MovePrintf(3, 0, "Target between taps: %5.2f(+/-25) ms", deviation)
+		text.MovePrintf(5, 0, "Current 1/4 note stream speed: %6.2f bpm", tpm/4)
+		text.MovePrintf(7, 0, "Time between taps:")
+		text.Refresh()
+
+		handleInput(text, &zcount, &xcount, &row, &start, &lastTap, &gap)
+		totaltaps = float64(zcount + xcount)
+		tpm = totaltaps/time.Now().Sub(start).Minutes()
+		if gap < deviation+25 && gap > deviation-25 {
+			grade = "good"
+		} else {
+			grade = "bad"
+		}
+		text.MovePrintf(8 + row, 0, "%8.2f ms [%s]", gap, grade)
+		row++
+		if row >= 50 {
+			row = 0
+		}
+	}
+}
+
+func handleInput(text *gc.Window, zcount, xcount, row *int, start, lastTap *time.Time, gap *float64) {
 	key := text.GetChar()
 
 	switch byte(key) {
@@ -83,6 +175,7 @@ func handleInput(text *gc.Window, zcount, xcount *int, start, lastTap *time.Time
 		*gap = 0
 		*zcount = 0
 		*xcount = 0
+		*row = 0
 		text.Erase()
 	}
 }
